d1f1fb95add629e535e72497a09a7e5e6a77f0ff
Tony B <neumanntruman@gmail.com>
2025-06-04 15:58:09 -0700

    Set Flask to bind to 0.0.0.0 and use PORT

--- app.py
+++ app.py
@@ -348,4 +348,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         load_sample_marketplace_items()
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
